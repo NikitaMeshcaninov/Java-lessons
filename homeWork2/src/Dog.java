ea91695d459211feed8803fb90051f32449f9582
NIkita Meshchaninov <meshn@outlook.com>
2016-06-09 20:28:05 +0300

    second comit for HW2

---
+++ homeWork2/src/Dog.java
@@ -0,0 +1,13 @@
+
+public class Dog {
+    public String name;
+    public String says;
+
+    public void name() {
+        System.out.print(name);
+    }
+
+    public void says() {
+        System.out.print(" syas," + says + "   ");
+    }
+}
