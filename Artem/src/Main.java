cfc107deae4436a304a4ab65f4a87dd20115283e
NIkita Meshchaninov <meshn@outlook.com>
2016-06-21 10:19:37 +0300

    ???

---
+++ Artem/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        WorkWithList arrayListThatWillBeOurObject = new WorkWithList();
+
+        arrayListThatWillBeOurObject.transformerArrayToArrayList();
+        arrayListThatWillBeOurObject.ourMax();
+        arrayListThatWillBeOurObject.Sorter();
+        arrayListThatWillBeOurObject.emptyArrayList();
+
+
+    }
+}
